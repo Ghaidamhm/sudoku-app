a60cd39b6489f2adf1663f7f1fea670bf9814982
BushraAlzahrani <102851258+BushraAlzahrani@users.noreply.github.com>
2022-10-19 21:46:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,63 @@
-import 'package:flutter/material.dart';  
-  
-void main() {runApp(MyApp());}  
-  
-class MyApp extends StatelessWidget {  
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
-        home: Scaffold(appBar: AppBar(  
-          title: Text("Flutter GridView Demo"),  
-        ),  
-            body: GridView.count(  
-                crossAxisCount: 3,  
-                crossAxisSpacing: 4.0,  
-                mainAxisSpacing: 8.0,  
-                children: List.generate(choices.length, (index) {  
-                  return Center(  
-                    child: SelectCard(choice: choices[index]),  
-                  );  
-                }  
-                )  
-            )  
-        )  
-    );  
-  }  
-}  
-  
-class Choice {  
-  const Choice({this.title, this.icon});  
-  final String? title;  
-  final IconData? icon;  
-}  
-  
-const List<Choice> choices = const <Choice>[  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
-  const Choice(title: ''),  
+import 'dart:math';
 
-];  
-  
-class SelectCard extends StatelessWidget {  
-  const SelectCard({Key? key, required this.choice}) : super(key: key);  
-  final Choice choice;  
-  
-  @override  
-  Widget build(BuildContext context) {  
-    final TextStyle? textStyle = Theme.of(context).textTheme.displaySmall;  
-    return Card(  
-        color: Color.fromARGB(255, 221, 218, 214),  
-        child: Center(child: Column(  
-            crossAxisAlignment: CrossAxisAlignment.center,  
-            children: <Widget>[  
-              Expanded(child: Icon(choice.icon, size:10.0, color: textStyle?.color)),  
-              Text(choice.title!, style: textStyle),  
-            ]  
-          
-        ),  
-        )  
-    );  
-  }  
-}  
+import 'package:flutter/material.dart';
+import './sudoku_box.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+
+ 
+generateCellNumbers(){
+
+
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Expanded(
+      
+      child: Column(
+        children: [
+          Row(children: [
+              SudokuBox(),
+              SudokuBox(),
+              SudokuBox(),
+
+          ],),
+             Row(children: [
+              SudokuBox(),
+              SudokuBox(),
+              SudokuBox(),
+
+          ],),
+             Row(children: [
+              SudokuBox(),
+              SudokuBox(),
+              SudokuBox(),
+
+          ],),
+        ],
+      )
+    ));
+  }
+}
